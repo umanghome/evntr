d8c8506023af8d53195ab4598503e9dadfd6db5c
Umang Galaiya <umang.galaiya@razorpay.com>
2018-02-11 03:04:46 +0530

    Basic event emitter.

---
+++ evntr.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/**
+ * An eventer.
+ */
+var Evntr = function Evntr() {
+  var c = {};
+
+  return {
+
+    /**
+     * Sets an event listener.
+     * @param {String} event Event name.
+     * @param {Function} listener Event listener.
+     */
+    on: function on(event, listener) {
+      // Sanity check.
+      if (!event || !listener) {
+        return;
+      }
+
+      // Initialise empty array for listeners if one does not exist.
+      if (!c[event]) {
+        c[event] = [];
+      }
+
+      // Set listener.
+      c[event].push(listener);
+    },
+
+    /**
+     * Emits an event with the given arguments.
+     * @param {String} event Event name.
+     */
+    emit: function emit(event) {
+      for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+        args[_key - 1] = arguments[_key];
+      }
+
+      // Sanity check.
+      if (!event || !c || !c[event]) {
+        return;
+      }
+
+      // Invoke each callback.
+      for (var i = 0; i < c[event].length; i++) {
+        c[event][i].apply(null, args);
+      }
+    },
+
+    /**
+     * Removes an event listener.
+     * @param {String} event Event name.
+     * @param {Function} listener Event listener.
+     */
+    off: function off(event, listener) {
+      // Sanity check.
+      if (!event || !listener || !c || !c[event]) {
+        return;
+      }
+
+      // Get index of listener and remove it.
+      var index = c[event].indexOf(listener);
+      if (index >= 0) {
+        c[event].splice(index, 1);
+      }
+    }
+
+  };
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = Evntr;
+} else {
+  if (typeof define === 'function' && define.amd) {
+    define([], function () {
+      return Evntr;
+    });
+  } else {
+    window.Evntr = Evntr;
+  }
+}
---
+++ index.js
@@ -0,0 +1,76 @@
+/**
+ * An eventer.
+ */
+const Evntr = function () {
+  const c = {};
+
+  return {
+    
+    /**
+     * Sets an event listener.
+     * @param {String} event Event name.
+     * @param {Function} listener Event listener.
+     */
+    on: (event, listener) => {
+      // Sanity check.
+      if (!event || !listener) {
+        return;
+      }
+
+      // Initialise empty array for listeners if one does not exist.
+      if (!c[event]) {
+        c[event] = [];
+      }
+
+      // Set listener.
+      c[event].push(listener);
+    },
+
+    /**
+     * Emits an event with the given arguments.
+     * @param {String} event Event name.
+     */
+    emit: (event, ...args) => {
+      // Sanity check.
+      if (!event || !c || !c[event]) {
+        return;
+      }
+
+      // Invoke each callback.
+      for (let i = 0; i < c[event].length; i++) {
+        c[event][i].apply(null, args);
+      }
+    },
+
+    /**
+     * Removes an event listener.
+     * @param {String} event Event name.
+     * @param {Function} listener Event listener.
+     */
+    off: (event, listener) => {
+      // Sanity check.
+      if (!event || !listener || !c || !c[event]) {
+        return;
+      }
+      
+      // Get index of listener and remove it.
+      const index = c[event].indexOf(listener);
+      if (index >= 0) {
+        c[event].splice(index, 1);
+      }
+    }
+
+  };
+}
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = Evntr;
+} else {
+  if (typeof define === 'function' && define.amd) {
+    define([], function() {
+      return Evntr;
+    });
+  } else {
+    window.Evntr = Evntr;
+  }
+}
